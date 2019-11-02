88cec35c16b44528a2a8fc54d5e9de53326f61eb
HappehBunneh <disgracedandbetrayed@gmail.com>
2019-11-02 10:29:22 +0000

    #WhiteFlag

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__, template_folder="build/", static_folder="build/static")
 
 @app.route('/')
 def hello_world():
